b302ba94e633e86df66af7f45e609c3d9d0ba8e6
siddharthgouthaman <sidcse2309@gmail.com>
2025-02-22 15:06:31 +0530

    errors fixed

--- challenge-1/app.js
+++ challenge-1/app.js
@@ -1,19 +1,21 @@
 const togglebutton=document.getElementById("toggleButton");
 const bulb=document.getElementById("bulb");
-const switchstatus=document.getElementsById("status");
-const bgcolor=document.body.style.backgroundColor;
+const switchStatus = document.getElementById('status');
+
 togglebutton.addEventListener('click',()=>{
+    let bgcolor=document.body.style.backgroundColor;
     if(!bgcolor || bgcolor=="white"){
+
         togglebutton.innerText="Turn Off";
-        switchstatus.innerText="Status: On";
-        bulb.setAttribute('class',bulb);
+        switchStatus.innerText="Status: On";
+        bulb.setAttribute('class','bulb');
         document.body.style.backgroundColor="black";
     
     }
     else  if( bgcolor=="black"){
         togglebutton.innerText="Turn On";
-        switchstatus.innerText="Status: Off";
-        bulb.setAttribute('class',bulb.Off);
+        switchStatus.innerText="Status: Off";
+        bulb.setAttribute('class','bulb Off');
         document.body.style.backgroundColor="white";
     
     }
