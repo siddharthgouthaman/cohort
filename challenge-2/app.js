b956f9bdffcd2517da8949ebb0054dee88f500aa
siddharthgouthaman <sidcse2309@gmail.com>
2025-02-22 15:45:37 +0530

    Update app.js

--- challenge-2/app.js
+++ challenge-2/app.js
@@ -1,3 +1,25 @@
 /**
  * Write your challenge solution here
  */
+const mainheading=document.getElementById("mainHeading");
+const redbtn=document.getElementById("redButton");
+const greenbtn=document.getElementById("greenButton");
+const bluebtn=document.getElementById("blueButton");
+const purplebtn=document.getElementById("purpleButton");
+const orginalbtn=document.getElementById("resetButton");
+
+redbtn.addEventListener('click',()=>{
+mainheading.style.color="#e74c3c"
+})
+greenbtn.addEventListener('click',()=>{
+  mainheading.style.color="#2ecc71"
+})
+bluebtn.addEventListener('click',()=>{
+  mainheading.style.color="#3498db"
+})
+purplebtn.addEventListener('click',()=>{
+  mainheading.style.color="#9b59b6"
+})
+orginalbtn.addEventListener('click',()=>{
+  mainheading.style.color="34495e"
+})
