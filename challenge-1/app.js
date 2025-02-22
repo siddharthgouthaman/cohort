8c046997a7c3077b028366e3a5403ba4647e3540
siddharthgouthaman <sidcse2309@gmail.com>
2025-02-22 13:37:45 +0530

    bulb

--- challenge-1/app.js
+++ challenge-1/app.js
@@ -1,3 +1,20 @@
-/**
- * Write your challenge solution here
- */
+const togglebutton=document.getElementById("toggleButton");
+const bulb=document.getElementById("bulb");
+const switchstatus=document.getElementsById("status");
+const bgcolor=document.body.style.backgroundColor;
+togglebutton.addEventListener('click',()=>{
+    if(!bgcolor || bgcolor=="white"){
+        togglebutton.innerText="Turn Off";
+        switchstatus.innerText="Status: On";
+        bulb.setAttribute('class',bulb);
+        document.body.style.backgroundColor="black";
+    
+    }
+    else  if( bgcolor=="black"){
+        togglebutton.innerText="Turn On";
+        switchstatus.innerText="Status: Off";
+        bulb.setAttribute('class',bulb.Off);
+        document.body.style.backgroundColor="white";
+    
+    }
+})
