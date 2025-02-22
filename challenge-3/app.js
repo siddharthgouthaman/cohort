bdafc19a3e877309a0ddcb44f16c004ed6f7e0db
siddharthgouthaman <sidcse2309@gmail.com>
2025-02-22 16:13:21 +0530

    Update app.js

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,3 +1,30 @@
 /**
  * Write your challenge solution here
  */
+const nameip=document.getElementById("nameInput");
+const jobip=document.getElementById("jobInput");
+const ageip=document.getElementById("ageInput");
+const bioip=document.getElementById("bioInput");
+
+
+const namedp=document.getElementById("nameDisplay");
+const jobdp=document.getElementById("jobDisplay");
+const agedp=document.getElementById("ageDisplay");
+const biodp=document.getElementById("bioDisplay");
+
+nameip.addEventListener('input',()=>{
+  namedp.innerText=nameip.value||"not provided";
+  
+})
+jobip.addEventListener('input',()=>{
+  jobdp.innerText=jobip.value||"not provided";
+  
+})
+ageip.addEventListener('input',()=>{
+  agedp.innerText=ageip.value||"not provided";
+  
+})
+bioip.addEventListener('input',()=>{
+  biodp.innerText=bioip.value||"not provided";
+  
+})
